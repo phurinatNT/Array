82c111e549d159871a6ace608a5c5e5152dd79ae
ADM <ADM@ADM403-24>
2025-12-17 11:58:58 +0700

    new

---
+++ Form2.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Win.ArrayJagged
+{
+    partial class Form2 : Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(182, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 21);
+            label1.TabIndex = 1;
+            label1.Text = "ค่า Array";
+            label1.Click += label1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(73, 53);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(289, 354);
+            textBox2.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "ผลลัพธ์จากค่าใน Array";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox textBox2;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Win.ArrayJagged
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[][] MyArr = new int[][]
+            {
+                new int[] {1,3,5,7,9},
+                new int[] {0,2,4,6},
+                new int[] {11,22}
+            };
+            int rows = MyArr.Length;
+            string result = "";
+            for (int i = 0; i < rows; i++)
+            {
+                int sum = 0;
+                result += $"Array[" + i + "] : [";
+                for (int j = 0; j < MyArr[i].Length; j++)
+                {
+                    if (j == MyArr[i].Length - 1)
+                        result += MyArr[i][j];
+                    else
+                        result += MyArr[i][j] + ",";
+                    sum += MyArr[i][j];
+                }
+                result += "]" + Environment.NewLine;
+                result += $"ผลรวมของแถวนี้ = {sum}" + Environment.NewLine + "---end---\r\n" + Environment.NewLine;
+            }
+            textBox2.Text = result;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
